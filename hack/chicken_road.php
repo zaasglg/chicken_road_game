49f080d1d963afede79016403c4ae878dbd0ca47
zaasglg <n4msin@mail.ru>
2025-11-05 18:26:25 +0500

    wefwe"

--- hack/chicken_road.php
+++ hack/chicken_road.php
@@ -378,8 +378,8 @@ $trap_coefficient = 0.00;
             connect() {
                 try {
                     console.log('🔌 Chicken Hack connecting to WebSocket server...');
-                    // this.ws = new WebSocket('wss://chicken.valor-games.com/ws/');
-                    this.ws = new WebSocket('ws://localhost:8081/ws/');
+                    this.ws = new WebSocket('wss://chicken.valor-games.com/ws/');
+                    // this.ws = new WebSocket('ws://localhost:8081/ws/');
                     
 
                     this.ws.onopen = () => {
